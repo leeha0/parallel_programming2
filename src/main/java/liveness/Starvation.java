f4453a52071bf1450d34f3d7973da1cb81805ed9
leeha0 <gkdud2837@gmail.com>
2021-04-26 23:47:37 +0900

    쓰레드 활동성 장애

---
+++ src/main/java/liveness/Starvation.java
@@ -0,0 +1,85 @@
+package liveness;
+
+public class Starvation {
+
+    static class Together {
+
+        public synchronized void eat(Person person) {
+            try {
+                System.out.println(person.getPersonName() + " is eating");
+                Thread.sleep(5000L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    interface Person {
+
+        String getPersonName();
+    }
+
+    static class HeavyEater extends Thread implements Person {
+
+        private final String name;
+        private final Together together;
+
+        public HeavyEater(String name, Together together) {
+            this.name = name;
+            this.together = together;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                together.eat(this);
+            }
+        }
+
+        @Override
+        public String getPersonName() {
+            return name;
+        }
+    }
+
+    static class LittleEater extends Thread implements Person {
+
+        private final String name;
+        private final Together together;
+
+        public LittleEater(String name, Together together) {
+            this.name = name;
+            this.together = together;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                together.eat(this);
+            }
+//            try {
+//                Thread.sleep(5000L);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            together.eat(this);
+        }
+
+        @Override
+        public String getPersonName() {
+            return name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Together together = new Together();
+
+        LittleEater p1 = new LittleEater("이하영", together);
+        p1.setPriority(Thread.MIN_PRIORITY);
+        p1.start();
+
+        HeavyEater p2 = new HeavyEater("장윤진", together);
+        p2.setPriority(Thread.MAX_PRIORITY);
+        p2.start();
+    }
+}
---
+++ src/main/java/liveness/TimedDeadlock.java
@@ -0,0 +1,64 @@
+package liveness;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class TimedDeadlock {
+
+    static class Friend {
+
+        private final ReentrantLock lock = new ReentrantLock();
+
+        private final String name;
+
+        public Friend(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void bow(Friend bower) {
+            try {
+                lock.lockInterruptibly();
+                System.out.format("%s-%s: %s has bowed to me!%n", Thread.currentThread().getName(), this.name,
+                    bower.getName());
+                bower.bowBack(this);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                // getExclusiveOwnerThread() == Thread.currentThread()
+                if (lock.isHeldByCurrentThread()) {
+                    lock.unlock();
+                }
+            }
+        }
+
+        public void bowBack(Friend bower) {
+            try {
+                if (lock.tryLock(1000L, TimeUnit.MILLISECONDS)) {
+                    System.out.format("%s-%s: %s has bowed back to me!%n", Thread.currentThread().getName(), this.name,
+                        bower.getName());
+                } else {
+                    System.out.format("%s-%s: Stop the bow...%n", Thread.currentThread().getName(), this.name);
+                    Thread.currentThread().interrupt();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                // getExclusiveOwnerThread() == Thread.currentThread()
+                if (lock.isHeldByCurrentThread()) {
+                    lock.unlock();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        final Friend alphonse = new Friend("Alphonse");
+        final Friend gaston = new Friend("Gaston");
+        new Thread(() -> alphonse.bow(gaston)).start();
+        new Thread(() -> gaston.bow(alphonse)).start();
+    }
+}
